db3c5e83c217b5a1934a2864c4cba3bbefd47aed
ZertDS640 <133051141+ZertDS640@users.noreply.github.com>
2023-05-16 14:19:00 -0500

    Add files via upload

---
+++ TEIKOK/AcessoDatosD/ClientesD.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcessoDatosD
+{
+    public class ClientesD
+    {
+    
+
+    public bool? InsertaClienteD(String @strCedula, String @strNombre, String @strApellido, String @strTelefono,
+        String @strDireccion, String @strEmail)
+        {
+            bool? respuesta = false;
+            try
+            {
+                using (EJEMPLO1Entities context = new AcessoDatosD.EJEMPLO1Entities())
+                {
+                    var sp = context.sp_insertar_clientes(strCedula, strNombre, strApellido,
+                                                         strTelefono, strDireccion, strEmail);
+                    foreach (var item in sp)
+                    {
+                        respuesta = item.Respuesta;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return respuesta;
+        }
+    }
+}
+
+
---
+++ TEIKOK/AcessoDatosD/ProductosD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AcessoDatosD
+{
+    public class ProductosD
+    {
+
+
+        public bool? InsertaProductosD(String @strNombreProducto, decimal @decPrecio, int @intCategoria)
+        {
+            bool? respuesta = false;
+            try
+            {
+                using (EJEMPLO1Entities context = new AcessoDatosD.EJEMPLO1Entities())
+                {
+                    var sp = context.sp_inserta_productos(strNombreProducto, decPrecio, @intCategoria);
+                    foreach (var item in sp)
+                    {
+                        respuesta = item.Respuesta;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return respuesta;
+        }
+    }
+}
+
+
+
