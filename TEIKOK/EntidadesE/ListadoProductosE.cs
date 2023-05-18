fa53880735ce411beed7014e5dca44ed9eec97d3
ZertDS640 <133051141+ZertDS640@users.noreply.github.com>
2023-05-17 21:35:39 -0500

    Add files via upload

---
+++ TEIKOK/EntidadesE/ListadoProductosE.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesE
+{
+    public class ListadoProductosE
+    {
+        public string NombreProducto { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public int Categoria { get; set; }
+
+        public int CodigoProducto { get; set; }
+
+    }
+}
